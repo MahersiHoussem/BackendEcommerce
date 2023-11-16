08a280f8a960122d9ce28dff7e013a98ccd13ecc
MahersiHoussem <mhythpussem21@gmail.com>
2023-11-16 21:07:58 +0100

    new version

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 const express=require('express');
-const mongoose =require("mongoose")
-const dotenv =require('dotenv');
-dotenv.config()
-const app = express();
-const categorieRouter =require("./routes/categorie.route");
+const  mongoose =require("mongoose")
+const dotenv =require('dotenv')
+const categorieRouter=require("./routes/categorie.route")
 const scategorieRouter=require("./routes/scategorie.route")
 const articleRouter=require("./routes/article.route")
-//BodyParser Middleware
-app.use(express.json()); 
+dotenv.config()
+const app = express();
+app.use(express.json())
 
+mongoose.set("strictQuery", false);
 // Connexion à la base données
 mongoose.connect(process.env.DATABASECLOUD,{
- useNewUrlParser: true,
- useUnifiedTopology: true
- })
- .then(() => {console.log("Connexion à la base de données réussie");
+useNewUrlParser: true,
+useUnifiedTopology: true
+})
+.then(() => {console.log("Connexion à la base de données réussie");
 }).catch(err => {
- console.log('Impossible de se connecter à la base de données', err);
- process.exit();
+console.log('Impossible de se connecter à la base de données', err);
+process.exit();
 });
 
-app.get("/",(req,res)=>{
-res.send("bonjour");
-});
-app.use("/api/categories",categorieRouter);
+app.use("/api/categories",categorieRouter)
 app.use("/api/scategories",scategorieRouter)
 app.use("/api/articles",articleRouter)
 app.listen(process.env.PORT, () => {
+    
  console.log(`Server is listening on port ${process.env.PORT}`); });
  module.exports = app;
